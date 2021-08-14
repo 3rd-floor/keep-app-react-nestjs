b332110fa36ace53b21aae26d7500559a161fb38
Tairone Dias <taironecdias@gmail.com>
2021-08-14 09:57:09 -0300

    Adicionando a diretiva de Optional para que seja permitido alterar apenas um atributo e não todos

--- api/src/modules/notes/dto/update-note.dto.ts
+++ api/src/modules/notes/dto/update-note.dto.ts
@@ -1,9 +1,11 @@
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class UpdateNoteDTO {
+  @IsOptional()
   @IsNotEmpty()
   @IsString()
   title: string;
+  @IsOptional()
   @IsNotEmpty()
   @IsString()
   description: string;

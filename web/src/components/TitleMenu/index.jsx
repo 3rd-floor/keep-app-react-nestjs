3dbea45991f2283a47c3a70214fb7aec609cbf9a
marcelobiao <biao.marcelo@gmail.com>
2021-08-17 16:26:12 -0300

    Criando componente para a barra de título

---
+++ web/src/components/TitleMenu/index.jsx
@@ -0,0 +1,13 @@
+function TitleMenu(){
+    return (
+        <div className='title-menu'>
+            <img src='' alt='logo'></img>
+            <div className='search-input'>
+                <input type='text'></input>
+                <button>Buscar</button>
+            </div>
+        </div>
+    );
+}
+
+export default TitleMenu;

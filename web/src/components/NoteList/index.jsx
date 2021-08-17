caa071bbdeded3b0725bbf85c16b9898a2e9f259
marcelobiao <biao.marcelo@gmail.com>
2021-08-17 16:26:23 -0300

    Criando componente para a lista de cards

---
+++ web/src/components/NoteList/index.jsx
@@ -0,0 +1,24 @@
+import NoteCard from "../NoteCard";
+import './style.css';
+
+function NoteList(){
+    return (
+        <div className='notes-list'>
+            <NoteCard title='Card 1' body='is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+            <NoteCard title='Card 1' body='Texto Exemplo'/>
+        </div>
+    );
+}
+
+export default NoteList;
